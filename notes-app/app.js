b9cd5aafb634f84754312b372fa46766ae784fa3
arjun.amrith@gmail.com <amrith@tydy.it>
2019-04-17 22:01:13 +0530

    imported and used isEmail package from npm

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,10 @@
+var validator=require('validator')
+
 const add=require("./utils")
 const getNotes=require("./notes")
+
 console.log(add(1,3))
 const msg=getNotes()
 console.log(msg)
+
+console.log(validator.isEmail('example.com'))
