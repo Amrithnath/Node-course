090251990a6bc93d22d3c0bb68df54f68ce2e74b
Amrithnath <amrith@tydy.it>
2019-04-17 21:22:25 +0530

    The first challenge(append a line to notes.txt using fs.file append)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs=require('fs')
-fs.writeFileSync("notes.txt","My name is Amrithnath")
-
+// fs.writeFileSync("notes.txt","My name is Amrithnath")
+fs.appendFileSync("notes.txt"," I live in Bangalore")
+fs.appendFileSync("notes.txt","\n I was born on a Monday")
