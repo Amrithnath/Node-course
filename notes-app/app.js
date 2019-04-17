e42a1fc0ef6eb43cf0dc37f945c859af64278f9f
Amrithnath <amrith@tydy.it>
2019-04-17 21:25:18 +0530

    Define name as variable and print(changed the old code as seen in tutorial

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,3 @@
-const fs=require('fs')
-// fs.writeFileSync("notes.txt","My name is Amrithnath")
-fs.appendFileSync("notes.txt"," I live in Bangalore")
-fs.appendFileSync("notes.txt","\n I was born on a Monday")
+const name ="Amrith"
+
+console.log(name)
