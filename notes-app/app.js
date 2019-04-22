f93e3cb7e53971c40a3348e2f307f9e5f307f269
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-22 19:32:04 +0530

    Used chalk library in chalk for second challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,7 @@ var validator=require('validator')
 
 const add=require("./utils")
 const getNotes=require("./notes")
+const chalk=require('chalk')
 
 console.log(add(1,3))
 const msg=getNotes()
@@ -9,3 +10,4 @@ console.log(msg)
 
 console.log(validator.isURL('https://www.google.com'))
 console.log(validator.isEmail('example@gmail.com'))
+console.log(chalk.dim.inverse.green.bgWhiteBright('Success'))
