118413a0c66100914911f0bb19b1ef1c4a785771
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-22 20:35:45 +0530

    Used yargs.argv  instead of argv (npm package)

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,6 +3,7 @@ var validator=require('validator')
 const add=require("./utils")
 const getNotes=require("./notes")
 const chalk=require('chalk')
+const yargs=require('yargs')
 
 console.log(add(1,3))
 const msg=getNotes()
@@ -11,4 +12,16 @@ console.log(msg)
 console.log(validator.isURL('https://www.google.com'))
 console.log(validator.isEmail('example@gmail.com'))
 console.log(chalk.dim.inverse.blue.bgWhiteBright('Error'))
-console.log(chalk.yellow(process.argv[2]))
+
+
+const command = process.argv
+
+console.log(chalk.yellow(command))
+console.log(chalk.green(JSON.stringify(yargs.argv)))
+
+if(command==="add"){
+    console.log(chalk.blue("adding note..."))
+}
+else if(command === "remove"){
+    console.log(chalk.red("removing note..."))
+}
