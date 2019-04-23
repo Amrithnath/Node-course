2ad88b52b44549a8caa05fcdff0bd490f0282a6a
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-23 20:37:44 +0530

    Effecient arrow function

--- playground/2-arrow-functions.js
+++ playground/2-arrow-functions.js
@@ -1,7 +1,10 @@
 // const square=function(number){
 //     return number*number
 // }
-const square = (num)=>{
-    return num*num
-}
+// const square = (num)=>{
+//     return num*num
+// }
+
+const square= (num)=>num*num
+
 console.log(square(9))
