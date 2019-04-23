ada595ff422cbbaa88bd416775470ed7c978f2d5
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-23 20:46:36 +0530

    Arrow functions dont bind to this(they bind to parents this values

--- playground/2-arrow-functions.js
+++ playground/2-arrow-functions.js
@@ -5,6 +5,20 @@
 //     return num*num
 // }
 
-const square= (num)=>num*num
+// const square= (num)=>num*num
 
-console.log(square(9))
+// console.log(square(9))
+
+const event={
+    name:"Tedx",
+    guestList:['Amrith','Ash','Cooke'],
+    printGuestList(){
+        console.log("Guest List for "+this.name)
+        this.guestList.forEach((guest)=>{
+            console.log(guest + ' is attending '+ this.name)
+        })
+    }
+    
+}
+
+event.printGuestList()
