37de4e85e9bb75aea8ab5f336735a2eff22acb9a
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-26 10:11:34 +0530

    Sample API call to Darksky API for current weather

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -10,6 +10,7 @@ API_key=process.env.key
 const URL="https://api.darksky.net/forecast/"+API_key+"/37.8267,-122.4233"
 
 request({url:URL},(error,response)=>{
-    console.log(response)
+    const data= JSON.parse(response.body)
+    console.log(data.currently)
 }
 )
