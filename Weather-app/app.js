67d21e55cfca05d9413f001f1e57542d680c2be4
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-30 09:12:43 +0530

    add weather summary

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -7,11 +7,11 @@ dotenv.config();
 API_key=process.env.key
 
 
-const URL="https://api.darksky.net/forecast/"+API_key+"/37.8267,-122.4233"
+const URL="https://api.darksky.net/forecast/"+API_key+"/37.8267,-122.4233?units=si"
 
 request({url:URL, json:true},(error,response)=>{
     temp=response.body.currently["temperature"]
     rainP=response.body.currently["precipProbability"]
-    console.log("It is currently "+temp+" degrees outside and there is a "+rainP+" chance of rain")
+    console.log(response.body.daily.data[0].summary+"It is currently "+temp+" degrees Celsius outside and there is a "+rainP+"% chance of rain")
 }
 )
