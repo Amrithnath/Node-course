4bbc8045516b27e2e28533d325a229f659c543c9
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-04-30 09:06:09 +0530

    proper response formatting

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -9,8 +9,9 @@ API_key=process.env.key
 
 const URL="https://api.darksky.net/forecast/"+API_key+"/37.8267,-122.4233"
 
-request({url:URL},(error,response)=>{
-    const data= JSON.parse(response.body)
-    console.log(data.currently)
+request({url:URL, json:true},(error,response)=>{
+    temp=response.body.currently["temperature"]
+    rainP=response.body.currently["precipProbability"]
+    console.log("It is currently "+temp+" degrees outside and there is a "+rainP+" chance of rain")
 }
 )
