742e89018425beeb193461a381ad1da37d2e1ca7
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-02 11:09:53 +0530

    implementing Async callback functions

---
+++ playground/4-callback.js
@@ -0,0 +1,21 @@
+setTimeout(() =>{
+    console.log("two seconds are up")
+},2000)
+const names=["Amrith","Riny","Ash"]
+const shortNames=names.filter((names)=>{
+    return names.length <= 4
+})
+console.log(shortNames)
+
+const geocode =(address,callback)=>{
+    setTimeout(()=>{
+        const data={
+            latitude:'0',
+            longitude:'0'
+        }
+        callback(data)
+    },2000)
+}
+geocode('Philadelphia',(data)=>{
+    console.log(data)
+})
