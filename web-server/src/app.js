6c6e3b6b4491dfb471bd27b39b48442da3b31140
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-03 18:34:30 +0530

    Used a handlebar file to keep a uniform website template
    Co-authored-by:arjunamrith<amrith@tydy.it>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,15 @@ const express=require('express')
 
 const app=express()
 const publicpath=path.join(__dirname,'../Public')
-app.use(express.static(publicpath))
 
+app.use(express.static(publicpath))
+app.set('view engine','hbs')
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:"Weather"
+    })
+})
 app.get('/weather',(req,res)=>{
     res.send({
         Forecast:"It is currently 30 degrees celsius with a 0% chance of rain",
