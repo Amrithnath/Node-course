2b8718b4c615bd550d36e1feeed55a60b26314ea
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-03 17:43:47 +0530

    Challenge completed But to access we have to append .html to access it
    Co-authored-by:arjunamrith<amrith@tydy.it>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,28 +5,14 @@ const app=express()
 
 app.use(express.static(path.join(__dirname,'../Public')))
 
-app.get('/help',(req,res)=>{
-    res.send('<h1>Help</h1>')
-})
-app.get('/about',(req,res)=>{
-    res.send("<h1>About page</h1>")
-})
+app.use(express.static(path.join(__dirname,'../Public')))
+app.use(express.static(path.join(__dirname,'../Public')))
 app.get('/weather',(req,res)=>{
     res.send({
         Forecast:"It is currently 30 degrees celsius with a 0% chance of rain",
         location:"bangalore"
     })
 })
-/*
-Challenge
-Goal:Create Two more HTML files
-1.Create a HTML page for about with about title
-2.Create HTML page for help with help title
-3.Remove old route handlers
-4.Visit both in the browser
-*/
-
-
 
 app.listen(3000,()=>{
     console.log("Server is up on 3000")
