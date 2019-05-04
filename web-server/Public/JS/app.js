93f4c5fe76062d5e93b5b651f9ba410839a4ac98
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-04 15:58:55 +0530

    Challenge complete
    Knocked it out of the park
    
    Co-authored-by:arjunamrith<amrith@tydy.it>

--- web-server/Public/JS/app.js
+++ web-server/Public/JS/app.js
@@ -1,16 +1,17 @@
-
-fetch('http://localhost:3000/weather?address=bosonsdaskasd').then((response)=>{
-    console.log(response)
-    response.json().then((data)=>{
-        if(data.error){
-            error=data.error
-            console.log({error})
-        }
-        else{
-            console.log(data)
-        }
+function send(location){
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+        console.log(response)
+        response.json().then((data)=>{
+            if(data.error){
+                error=data.error
+                console.log({error})
+            }
+            else{
+                console.log(data)
+            }
+        })
     })
-})
+}
 
 
 const weatherform = document.querySelector('form')
@@ -18,16 +19,5 @@ const search = document.querySelector('input')
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
-    console.log(location)
+    send(location)
 })
-
-
-/*
-Challenge
-
-Goal:use the input value to get the weather
-
-1. Migrate the fetch call to the submit function
-2. Use search text as the address query string value
-3.submit form with a valid and invalid value to test
-*/
