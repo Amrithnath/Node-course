3fd62a4349326f36b6f4b95e755bbdfd2bb725ca
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-17 20:00:06 +0530

    Added new challenge for task manager app
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/mongodb.js
+++ taskmanager/mongodb.js
@@ -14,8 +14,25 @@ MongoClient.connect(connectionurl,{useNewUrlParser : true},(error,client)=>{
 
     const db=client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name:'amrithnath',
-        age:25
+//     db.collection('users').insertOne({
+//         name:'amrithnath',
+//         age:25
+//     },(error,result)=>{
+//         if (error) console.log("Unable to insert user")
+//         console.log(result.ops)
+//     })
+//
+    db.collection('users').insertMany([
+        {
+            name:"Yvette",
+            age:25
+        },
+        {
+            name:"Yvonne",
+            age:48
+        }
+    ],(error,response)=>{
+        if(error) console.log("Unable to insert documents")
+        console.log(response.ops)
     })
 })
