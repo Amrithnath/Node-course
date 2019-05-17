774c8a2715cd7c285decd34cd0ee06bc4a0bdb29
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-17 21:47:53 +0530

    refactored code and added minimal documentation for readability
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/mongodb.js
+++ taskmanager/mongodb.js
@@ -1,9 +1,5 @@
 //CRUD operations
 
-
-// const mongodb = require('mongodb')
-//const MongoClient = mongodb.MongoClient
-// const ObjectID=
 const {
     MongoClient,
     ObjectID
@@ -20,83 +16,77 @@ MongoClient.connect(connectionurl, {
     }
 
     const db = client.db(databaseName)
+    //Read one
+    db.collection('users').findOne({
+        name: 'Yvette'
+    }, (error, response) => {
+        if (error) console.log("Couldnt find said user")
+        console.log(response)
+    })
+    //Read many
+    db.collection('users').find({
+        age: 25
+    }).toArray((error, users) => {
+        if (error) console.log("Couldnt fin users matching criteria")
+        console.log(users)
+    })
+    db.collection('users').find({
+        age: 25
+    }).count((error, count) => {
+        if (error) console.log("Couldnt fin users matching criteria")
+        console.log(count)
+    })
 
-    // db.collection('users').findOne({
-    //     name: 'Yvette'
-    // }, (error, response) => {
-    //     if (error) console.log("Couldnt find said user")
-    //     console.log(response)
-    // })
-    // db.collection('users').find({
-    //     age: 25
-    // }).toArray((error, users) => {
-    //     if (error) console.log("Couldnt fin users matching criteria")
-    //     console.log(users)
-    // })
-    // db.collection('users').find({
-    //     age: 25
-    // }).count((error, count) => {
-    //     if (error) console.log("Couldnt fin users matching criteria")
-    //     console.log(count)
-    // })
-
-    // db.collection('tasks').findOne({
-    //     _id: new ObjectID("5cdec618fb182e3c9c57f95b")
-    // }, (error, task) => {
-    //     if (error) console.log("please check your _id")
-    //     console.log(task)
-    // })
-    // db.collection('tasks').find({
-    //     completed: false
-    // }).toArray((error, tasks) => {
-    //     if (error) console.log("Hurray not Incomplete tasks")
-    //     console.log(tasks)
-    // })
-    // db.collection('users').updateOne({
-    //     _id: new ObjectID("5cdec8b689aafe4e4491ed40")
-    // }, {
-    //     $inc:{
-    //         age:1
-    //     }
-    // }).then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-    // db.collection('tasks').updateMany({
-    //     completed: false
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }).then((result) => {
-    //     console.log(result.modifiedCount)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-    // db.collection('users').deleteMany({
-    //     age:48
-    // }).then((Response)=>{
-    //     console.log(Response.deletedCount)
-    // }).catch((error)=>{
-    //     console.log(error)
-    // })
-
+    db.collection('tasks').findOne({
+        _id: new ObjectID("5cdec618fb182e3c9c57f95b")
+    }, (error, task) => {
+        if (error) console.log("please check your _id")
+        console.log(task)
+    })
+    db.collection('tasks').find({
+        completed: false
+    }).toArray((error, tasks) => {
+        if (error) console.log("Hurray not Incomplete tasks")
+        console.log(tasks)
+    })
+    //update one
+    db.collection('users').updateOne({
+        _id: new ObjectID("5cdec8b689aafe4e4491ed40")
+    }, {
+        $inc: {
+            age: 1
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+    //update many
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result.modifiedCount)
+    }).catch((error) => {
+        console.log(error)
+    })
+    //delete many
+    db.collection('users').deleteMany({
+        age: 48
+    }).then((Response) => {
+        console.log(Response.deletedCount)
+    }).catch((error) => {
+        console.log(error)
+    })
+    //delete one
     db.collection('tasks').deleteOne({
-        description:"make food"
-    }).then((result)=>{
+        description: "make food"
+    }).then((result) => {
         console.log(result.deletedCount)
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log(error)
     })
 })
-
-/*
-    challenge 
-    Goal Use delete one to remove a task
-
-    1. Grab the description of the task you want to delete
-    2. setup the call with a deleteone query to delete the task above
-    3. use promises with success or error handlers
-    4. test
-*/
