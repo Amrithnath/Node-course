bb65db8437d9114104ca6bd513c0dc20bbb3de32
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-17 20:05:39 +0530

    inserts tasks to DB
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/mongodb.js
+++ taskmanager/mongodb.js
@@ -4,35 +4,54 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionurl='mongodb://127.0.0.1:27017'
-const databaseName='task-manager'
+const connectionurl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
 
-MongoClient.connect(connectionurl,{useNewUrlParser : true},(error,client)=>{
-    if(error){
+MongoClient.connect(connectionurl, {
+    useNewUrlParser: true
+}, (error, client) => {
+    if (error) {
         return console.log('unable to connect to DB')
     }
 
-    const db=client.db(databaseName)
+    const db = client.db(databaseName)
 
-//     db.collection('users').insertOne({
-//         name:'amrithnath',
-//         age:25
-//     },(error,result)=>{
-//         if (error) console.log("Unable to insert user")
-//         console.log(result.ops)
-//     })
-//
-    db.collection('users').insertMany([
+    //     db.collection('users').insertOne({
+    //         name:'amrithnath',
+    //         age:25
+    //     },(error,result)=>{
+    //         if (error) console.log("Unable to insert user")
+    //         console.log(result.ops)
+    //     })
+    //
+    // db.collection('users').insertMany([
+    //     {
+    //         name:"Yvette",
+    //         age:25
+    //     },
+    //     {
+    //         name:"Yvonne",
+    //         age:48
+    //     }
+    // ],(error,response)=>{
+    //     if(error) console.log("Unable to insert documents")
+    //     console.log(response.ops)
+    // })
+    db.collection('tasks').insertMany([
         {
-            name:"Yvette",
-            age:25
+            description:"Water plants",
+            completed: true
         },
         {
-            name:"Yvonne",
-            age:48
+            description:"Write code",
+            completed:false
+        },
+        {
+            description:"make food",
+            completed:true
         }
     ],(error,response)=>{
-        if(error) console.log("Unable to insert documents")
+        if(error) console.log("couldnt insert tasks")
         console.log(response.ops)
     })
 })
