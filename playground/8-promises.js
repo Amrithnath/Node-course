7bcfdaba756a9d50ff5f465c7b5d7c985dd93fd4
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-20 22:42:48 +0530

    Promise chaining
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: Arjunamrith <amrith@tydy.it>

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,13 +1,42 @@
-const doworkpromise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        // resolve([1,2,3])
-        reject( "Things went wrong")
-    },2000)
-})
+// import { rejects } from "assert";
 
-doworkpromise.then((result)=>{
-    console.log("success")
-    console.log(result)
-}).catch((error)=>{
-    console.log(error)
+// const doworkpromise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         // resolve([1,2,3])
+//         reject( "Things went wrong")
+//     },2000)
+// })
+
+// doworkpromise.then((result)=>{
+//     console.log("success")
+//     console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+const add =(a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },2000)
+    })
+}
+// add(1,2).then((ans)=>{
+//     console.log(ans)
+//     add(ans,5).then((sum)=>{
+//         console.log(sum)
+//     }).catch((e)=>{
+//         console.log(e)
+//     })
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+add(1,2).then((sum)=>{
+    console.log(sum)
+    return add(sum,4)
+}).then((sum2)=>{
+    console.log(sum2)
+}).catch((e)=>{
+    console.log(e)
 })
