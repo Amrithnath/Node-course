5e99edc0caa84abfc8d402ba187e1692f49ca488
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-05-20 22:05:04 +0530

    Added tasks endpoint and the command to automate db start
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/src/db/mongoose.js
+++ taskmanager/src/db/mongoose.js
@@ -1,21 +1,7 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:2717/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-
-const Task = mongoose.model('Tasks',{
-    description:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    completed:{
-        type:Boolean,
-        required:false,
-        default:false
-    }
-})
--- taskmanager/src/index.js
+++ taskmanager/src/index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 require("./db/mongoose")
 const User= require('./models/user')
+const Task = require('./models/task')
 
 const app=express()
 const port = process.env.PORT || 5000
@@ -12,6 +13,15 @@ app.post('/users',(req,res)=>{
         res.send(user)
     }).catch((err)=>{
         res.status(400).send(err)
+    })
+})
+
+app.post('/task',(req,res)=>{
+    const task=new Task(req.body)
+    task.save().then(()=>{
+        res.send(task)
+    }).catch((e)=>{
+        res.status(400).send(e)
     })
 })
 app.listen(port,()=>{
