92e7f7e64834375e28add877cf747ab4a90143c4
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-07-23 19:57:01 +0530

    completed challenge
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/playground/promise-chaining2.js
+++ taskmanager/playground/promise-chaining2.js
@@ -2,15 +2,26 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 
-Task.findByIdAndDelete('5ce2b244059e3e3974f24520').then((task)=>{
-    console.log(task)
-    return Task.countDocuments()
-}).then((count)=>{
+// Task.findByIdAndDelete('5ce2b244059e3e3974f24520').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments()
+// }).then((count)=>{
+//     console.log(count)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+
+const deleteTaskCount = async (id)=>{
+    var task = await Task.findByIdAndDelete(id)
+    var count = await Task.countDocuments()
+    return count
+}
+deleteTaskCount('5ce2bba09f14bd3970ee524a').then((count)=>{
     console.log(count)
 }).catch((e)=>{
     console.log(e)
 })
-
 
 /*
 
