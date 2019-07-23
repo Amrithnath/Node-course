91cdaf1c4a4f6815c9c372b1140c14c1c0cc32c7
arjun.amrith@gmail.com <arjun.amrith@gmail.com>
2019-07-23 19:52:33 +0530

    New async/await task
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>
    Co-authored-by: arjunamrith <amrith@tydy.it>

--- taskmanager/playground/promise-chaining.js
+++ taskmanager/playground/promise-chaining.js
@@ -4,15 +4,25 @@ const User= require('../src/models/user')
 
 // 5ce2b9df0eeed13790d12232
 
-
-User.findByIdAndUpdate('5ce2b9df0eeed13790d12232',{age:1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})
-}).then((results)=>{
-    console.log(results)
+const updateAgeAndCount = async (id,age)=>{
+    const user = await User.findByIdAndUpdate(id,{age})
+    const count = await User.countDocuments({age})
+    return count
+}
+updateAgeAndCount('5ce2b9df0eeed13790d12232',2).then((count)=>{
+    console.log(count)
 }).catch((e)=>{
     console.log(e)
 })
+
+// User.findByIdAndUpdate('5ce2b9df0eeed13790d12232',{age:1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// }).then((results)=>{
+//     console.log(results)
+// }).catch((e)=>{
+//     console.log(e)
+// })
 
 /*
 
--- taskmanager/playground/promise-chaining2.js
+++ taskmanager/playground/promise-chaining2.js
@@ -2,7 +2,7 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 
-Task.findByIdAndRemove('5ce2b244059e3e3974f24520').then((task)=>{
+Task.findByIdAndDelete('5ce2b244059e3e3974f24520').then((task)=>{
     console.log(task)
     return Task.countDocuments()
 }).then((count)=>{
@@ -10,3 +10,17 @@ Task.findByIdAndRemove('5ce2b244059e3e3974f24520').then((task)=>{
 }).catch((e)=>{
     console.log(e)
 })
+
+
+/*
+
+    Use Async/awwait
+
+        create delete task count as a async function
+            accept id of task to remove
+        use await to delete tasks and count up incomplete tasks
+        return count
+        call function and attach then/catch to log results
+        test
+
+*/
